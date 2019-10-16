48e1f6cc53ff9ce11a88bf50be5aba8e815e7fdf
hargunanie <56492079+hargunanie@users.noreply.github.com>
2019-10-15 17:15:57 -0700

    added guess.py

---
+++ guess.py
@@ -0,0 +1,25 @@
+import random
+comp_num=random.randint(1, 100)
+prompt='What is your guess? Between 1 and 100. '
+num_guesses=0
+while True:
+  person_guess=raw_input(prompt)
+  if (comp_num==int(person_guess)):
+    num_guesses=num_guesses+1
+    print "You're Correct!"
+    print 'Number of guesses: ',
+    print num_guesses
+    break 
+  elif (comp_num>int(person_guess)):
+    print 'Too low!'
+    num_guesses=num_guesses+1
+  else:
+    print 'Too High!'
+    num_guesses=num_guesses+1
+  if (num_guesses==5):
+    print 'game over'
+    print ' '
+    print 'The correct number was ',
+    print comp_num 
+    print 'Nice try!'
+    break
