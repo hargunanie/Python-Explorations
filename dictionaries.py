d7fbba360b195b41f4526a3560c7ba766f3bb338
Ethan <ethan.hargunani@outlook.com>
2020-07-22 19:17:10 -0700

    updated all programs to python 3 added dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+dog_ages = {'Tucker':5,'Tanner':9}
+print(dog_ages['Tucker'])
+
+if 'bubba' in dog_ages == False:
+    print('bubba is gone')
--- guess.py
+++ guess.py
@@ -7,22 +7,22 @@ while True:
   person_guess=raw_input(prompt)
   if (comp_num==int(person_guess)):
     num_guesses=num_guesses+1
-    print "You're Correct!"
-    print 'Number of guesses: ',
-    print num_guesses
+    print("You're Correct!")
+    print('Number of guesses: '),
+    print(num_guesses)
     break
   elif (comp_num>int(person_guess)):
-    print 'Too low!'
+    print('Too low!')
     num_guesses=num_guesses+1
   elif (int(person_guess)>100):
     raise RuntimeError('ur bad')
   else:
-    print 'Too High!'
+    print('Too High!')
     num_guesses=num_guesses+1
   if (num_guesses==5):
-    print 'game over'
-    print ' '
-    print 'The correct number was ',
-    print comp_num
-    print 'Nice try!'
+    print('game over')
+    print(' ')
+    print('The correct number was '),
+    print(comp_num)
+    print('Nice try!')
     break
