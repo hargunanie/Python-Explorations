64bb93c1ac5fbe238edbeca0895dd0e8cfe03f41
Ethan <ethan.hargunani@outlook.com>
2020-07-23 10:30:14 -0700

    added classes.py

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Planet:
+    def __init__(self):
+        self.name = 'Hoth'
+        self.radius = 200000
+        self.gravity = 5.5
+        self.system = 'Hoth'
+    
+#    def orbit
+
+
+
+aplanet = Planet()
+print(f'Name is: {aplanet.name}')
+print(f'Radius is: {aplanet.radius}')
+print(f'The gravity is: {aplanet.gravity}')
