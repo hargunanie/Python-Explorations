4fa27aef1a22c5c48494191f143b8281b1100e39
Ethan <ethan.hargunani@outlook.com>
2020-07-29 14:46:16 -0700

    Testing imports

---
+++ testimport.py
@@ -0,0 +1,9 @@
+from classes import Planet
+
+
+aplanet = Planet()
+print(f'Name is: {aplanet.name}')
+print(f'Radius is: {aplanet.radius}')
+print(f'The gravity is: {aplanet.gravity}')
+print(aplanet.orbit())
+print(aplanet.planetshape())
