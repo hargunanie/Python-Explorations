539c0cd327457a7220aa869df84832ed778f9429
Ethan <ethan.hargunani@outlook.com>
2020-08-12 18:58:15 -0700

    reformatted guess.py

--- guess.py
+++ guess.py
@@ -1,27 +1,28 @@
 import random
-comp_num=random.randint(1, 100)
-prompt='What is your guess? Between 1 and 100. '
-num_guesses=0
+
+comp_num = random.randint(1, 100)
+prompt = 'What is your guess? Between 1 and 100. '
+num_guesses = 0
 while True:
-  person_guess=input(prompt)
-  if (comp_num==int(person_guess)):
-    num_guesses=num_guesses+1
-    print("You're Correct!")
-    print('Number of guesses: '),
-    print(num_guesses)
-    break
-  elif (comp_num>int(person_guess)):
-    print('Too low!')
-    num_guesses=num_guesses+1
-  elif (int(person_guess)>100):
-    raise RuntimeError('ur bad')
-  else:
-    print('Too High!')
-    num_guesses=num_guesses+1
-  if (num_guesses==5):
-    print('game over')
-    print(' ')
-    print('The correct number was '),
-    print(comp_num)
-    print('Nice try!')
-    break
+    person_guess = input(prompt)
+    if comp_num == int(person_guess):
+        num_guesses = num_guesses + 1
+        print("You're Correct!")
+        print('Number of guesses: '),
+        print(num_guesses)
+        break
+    elif comp_num > int(person_guess):
+        print('Too low!')
+        num_guesses = num_guesses + 1
+    elif int(person_guess) > 100:
+        raise RuntimeError('ur bad')
+    else:
+        print('Too High!')
+        num_guesses = num_guesses + 1
+    if num_guesses == 5:
+        print('game over')
+        print(' ')
+        print('The correct number was '),
+        print(comp_num)
+        print('Nice try!')
+        break
