6ab1517701e0a84ef2ad3832095e038563a76115
Ethan <ethan.hargunani@outlook.com>
2020-08-13 13:31:31 -0700

    reformatted pets.py

--- pets.py
+++ pets.py
@@ -1,15 +1,15 @@
 prompt = 'Do you have any pets?'
 var = input(prompt)
-if(var == 'no'):
+if var == 'no':
     print('Okay')
     exit()
 secprompt = 'What kind of pets?'
 secva = input(secprompt)
-petinfo = [var,secva]
-if(var == 'y'):
-    if(secva == 'dog'):
+petinfo = [var, secva]
+if var == 'y':
+    if secva == 'dog':
         print('You do have a common pet, which is a dog.')
-    elif(secva == 'cat'):
+    elif secva == 'cat':
         print('You do have a common pet, which is a cat.')
     else:
         print("You don't have a common pet, which is a", secva)
