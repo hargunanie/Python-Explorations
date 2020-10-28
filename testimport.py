a4f35abcfcc42a9bf06fb24a198d8cc607d4d4bd
Ethan <ethan.hargunani@outlook.com>
2020-10-27 19:26:16 -0700

    test commit, checking ssh connection

--- testimport.py
+++ testimport.py
@@ -4,6 +4,6 @@ from classes import Planet
 aplanet = Planet()
 print(f'Name is: {aplanet.name}')
 print(f'Radius is: {aplanet.radius}')
-print(f'The gravity is: {aplanet.gravity}')
+print(f'The gravity is equal to {aplanet.gravity}')
 print(aplanet.orbit())
 print(aplanet.planetshape())
